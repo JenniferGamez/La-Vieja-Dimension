3dcc07d34bbbcbdabcb4dbfff95aa90a6158d28e
JenniferGamez <50212868+JenniferGamez@users.noreply.github.com>
2019-05-28 10:32:57 -0400

    Update QuienQuiereSer.py

--- QuienQuiereSer.py
+++ QuienQuiereSer.py
@@ -5,24 +5,31 @@
 import random
 import string 
 
-Jugador1 = str(raw_input("Ingrese el nombre del Jugador 1:"))
-Jugador2 = str(raw_input("Ingrese el nombre del Jugador 2:"))
+Jugador1 = str(raw_input("Ingrese el nombre del Jugador 1: "))
+Jugador2 = str(raw_input("Ingrese el nombre del Jugador 2: "))
 
-print("Jugador1 =" , Jugador1)
-print("Jugador2 =" , Jugador2)
+print "\nJugador1 = %s" %Jugador1
+print "Jugador2 = %s" %Jugador2
 
-def QueQuiereSer(Jugador1): #El jugador decide si quiere ser X y O
-	letra = ''
-	if not(letra == 'X' or letra == 'O'): #si la letra no es X y O hara que se siga pidiendo al usuario una X o una O
-		print(str(Jugador1) , 'Quiere ser la letra X o la letra O?')
-		letra = raw_input().upper() #Esto hara que se coloquen mayusculas las letras que introduzcan
+def QueQuiereSer(Jugador1, Jugador2): #El jugador decide si quiere ser X y O
+	while True:
+		try: 
+			letra = str(raw_input("%s Quiere ser la letra X o la letra O?: " %Jugador1)).upper() 
+			# letra = raw_input().upper() #Esto hara que se coloquen mayusculas las letras que introduzcan
+			assert(letra == "X" or letra == "O")
+		except:
+			print "Intente nuevamente."
+		finally:
+			if letra == 'X':
+				Asig_1 = "X" #Jugador1
+				Asig_2 = "O" #Jugador2
+				return Asig_1 , Asig_2
+			elif letra == 'O':
+				Asig_1 = "O" #Jugador1
+				Asig_2 = "X" #Jugador2
+				return Asig_1 , Asig_2 #estos me arrojan estas variables que deben volverse locales
+			
+#Variables locales de la asignacion de X y O
 
-		if letra == 'X':
-			print (str(Jugador1) ,'sera las X')
-			print (str(Jugador2) ,'sera las O')
-		else:
-			print (str(Jugador1) ,'sera las O')
-			print (str(Jugador2) ,'sera las X')
-
-QueQuiereSer(Jugador1)
-
+jug1, jug2 = QueQuiereSer(Jugador1,Jugador2)
+	
