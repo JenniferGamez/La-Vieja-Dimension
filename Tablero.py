30786c8452607830fd8107408f752a5b91791df1
JenniferGamez <50212868+JenniferGamez@users.noreply.github.com>
2019-05-28 11:01:53 -0400

    Update Tablero.py

--- Tablero.py
+++ Tablero.py
@@ -3,17 +3,22 @@
 
 # Definiendo el tablero
 import sys
+import time
 import os
-
+# Matriz Inicial
 def MostrarTab(N): # -> Procedimiento Tablero
+	# N es la dimension del Tablero N x N
 	def Tablero(N):
-		tab= []
+		tab= [] 				# Creamos un arreglo vacio, donde tendra como elementos otros arreglos
 		for i in range(0,N):
-			tab.append([0]*N)
+			tab.append([0]*N) 	# introdujendo arreglos vacios al arreglo creado
 		return tab
 
 	tablero = Tablero(N)
 	for i in range(0,N):
 		print tablero[i]	
-						
+							
 ###NOTA: por ser procedimiento solo se llama sin el print
+							##################################
+
+MostrarTab(DimensionesTab)
