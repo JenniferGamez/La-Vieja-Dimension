846424df056d5341b7474b36608121ced66db1ab
JenniferGamez <50212868+JenniferGamez@users.noreply.github.com>
2019-06-02 12:29:20 -0400

    Update Tablero.py

--- Tablero.py
+++ Tablero.py
@@ -21,3 +21,27 @@ def MostrarTablero(tab): # Mostrar elementos sin "" ni los [], y organizado por 
 
 MostrarTablero(tab)		 # Muestra el tablero en la shell 
 
+
+
+                ###### ESTOS ADJUNTADOS SON LOS EMPLEADOS
+  
+  # Definicion que crea los tableros N x N
+def Tablero_NxN(tamano_Tablero, n):
+	# entrada de la matriz (elementos a rellenar)
+	tablero = [[ n for z in range(tamano_Tablero)] for y in range(tamano_Tablero)]
+	return tablero
+
+# Super Tablero este contiene las dimensiones (conjunto de tablero)
+def Super_tablero(tamano):
+	dimensiones = [ z for z in range(tamano)] #Array que contedra los demas tableros
+	for i in dimensiones:
+		dimensiones[i] = Tablero_NxN(tamano, 0)
+		# Asigna n == 0, porque inicialmente la matriz esta vacia
+	return dimensiones+
+
+#IMPRIMIR EL TABLERO. PROCEDIMIENTO 
+def MostrarTablero(tab): # Mostrar elementos sin "" ni los [], y organizado por filas al imprimir
+# Aqui definimos un tablero
+	for fila in tab:
+		print fila # Join une los elementos del array
+
